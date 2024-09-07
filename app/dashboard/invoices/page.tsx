1fae6f49ddd7d806d476eccd2220d7994ddb572a
harriszintambila <bed-com-32-21@unima.ac.mw>
2024-09-07 22:57:34 +0200

    configured the database through .env.example, invoices/page.tsx, seeding

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,3 +1,8 @@
 export default function Page() {
-    return <p>Invoices Page</p>
+    return( 
+        <div className="flex-none w-full h-4 bg-blue-300">
+                <h1><strong>Inv</strong>oi<strong>ces</strong></h1>
+        </div>
+    
+    );
 }
